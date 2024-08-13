8acc252fa849c9a9cd9ee3482b0d51ffbb12e757
Guy Khazma <guykhag@gmail.com>
2024-08-13 16:08:34 -0400

    example of combining stats

--- spark/v3.5/spark/src/main/java/org/apache/iceberg/spark/actions/NDVSketchGenerator.java
+++ spark/v3.5/spark/src/main/java/org/apache/iceberg/spark/actions/NDVSketchGenerator.java
@@ -36,10 +36,12 @@ import org.apache.iceberg.relocated.com.google.common.collect.ImmutableMap;
 import org.apache.iceberg.relocated.com.google.common.collect.Maps;
 import org.apache.iceberg.spark.SparkReadOptions;
 import org.apache.iceberg.types.Types;
+import org.apache.iceberg.util.ArrayUtil;
 import org.apache.spark.sql.Column;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.functions;
 import org.apache.spark.sql.stats.ThetaSketchAggregator;
 
 public class NDVSketchGenerator {
@@ -50,12 +52,22 @@ public class NDVSketchGenerator {
 
   static List<Blob> generateNDVSketchesAndBlobs(
       SparkSession spark, Table table, Snapshot snapshot, Set<String> columns) {
-    Map<Integer, Sketch> columnToSketchMap = computeNDVSketches(spark, table, snapshot, columns);
-    return generateBlobs(table, columns, columnToSketchMap, snapshot);
+    Map<Integer, Object> minValues = Maps.newHashMap();
+    Map<Integer, Object> maxValues = Maps.newHashMap();
+    Map<Integer, Sketch> columnToSketchMap = computeNDVSketches(spark, table, snapshot,
+            columns, minValues, maxValues);
+    return generateBlobs(table, columns, columnToSketchMap,
+            minValues,
+            maxValues,
+            snapshot);
   }
 
   private static List<Blob> generateBlobs(
-      Table table, Set<String> columns, Map<Integer, Sketch> sketchMap, Snapshot snapshot) {
+      Table table, Set<String> columns,
+      Map<Integer, Sketch> sketchMap,
+      Map<Integer, Object> minValues,
+      Map<Integer, Object> maxValues,
+      Snapshot snapshot) {
     return columns.stream()
         .map(
             columnName -> {
@@ -70,18 +82,23 @@ public class NDVSketchGenerator {
                   snapshot.sequenceNumber(),
                   ByteBuffer.wrap(sketch.toByteArray()),
                   null,
-                  ImmutableMap.of(APACHE_DATASKETCHES_THETA_V1_NDV_PROPERTY, String.valueOf(ndv)));
+                  ImmutableMap.of(
+                          APACHE_DATASKETCHES_THETA_V1_NDV_PROPERTY, String.valueOf(ndv),
+                          "min", String.valueOf(minValues.get(field.fieldId())),
+                          "max", String.valueOf(maxValues.get(field.fieldId()))
+                  ));
             })
         .collect(Collectors.toList());
   }
 
   private static Map<Integer, Sketch> computeNDVSketches(
-      SparkSession spark, Table table, Snapshot snapshot, Set<String> columnsToBeAnalyzed) {
+      SparkSession spark, Table table, Snapshot snapshot, Set<String> columnsToBeAnalyzed,
+      Map<Integer, Object> minValues, Map<Integer, Object> maxValues) {
     Map<Integer, Sketch> sketchMap = Maps.newHashMap();
     String tableName = table.name();
     List<String> columns = ImmutableList.copyOf(columnsToBeAnalyzed);
 
-    Column[] aggregateColumns =
+    Column[] thetaSketchColumns =
         columns.stream()
             .map(
                 columnName -> {
@@ -90,19 +107,31 @@ public class NDVSketchGenerator {
                   return new Column(thetaSketchAggregator.toAggregateExpression());
                 })
             .toArray(Column[]::new);
+    Column[] minColumns =
+          columns.stream()
+                  .map(columnName -> new Column(functions.min(columnName).expr()))
+                  .toArray(Column[]::new);
+    Column[] maxColumns =
+          columns.stream()
+                  .map(columnName -> new Column(functions.max(columnName).expr()))
+                  .toArray(Column[]::new);
     Dataset<Row> sketches =
         spark
             .read()
             .format("iceberg")
             .option(SparkReadOptions.SNAPSHOT_ID, snapshot.snapshotId())
             .load(tableName)
-            .select(aggregateColumns);
+            .select(ArrayUtil.concat(Column.class,
+                    thetaSketchColumns, minColumns, maxColumns));
 
     Row rows = sketches.collectAsList().get(0);
     Schema schema = table.schemas().get(snapshot.schemaId());
     for (int i = 0; i < columns.size(); i++) {
       Types.NestedField field = schema.findField(columns.get(i));
       sketchMap.put(field.fieldId(), Sketches.wrapSketch(Memory.wrap((byte[]) rows.get(i))));
+      // TODO: shouldn't be done this way
+      minValues.put(field.fieldId(), rows.get(i + columns.size()));
+      maxValues.put(field.fieldId(), rows.get(i + columns.size()));
     }
     return sketchMap;
   }
